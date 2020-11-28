fac5b2aee59efc8c1e58c41aabe6da9cc9d59984
cumbercon97 <cumberson97@gmail.com>
2020-11-28 17:40:48 -0400

    Finally

--- Sorting.cpp
+++ Sorting.cpp
@@ -19,23 +19,22 @@ void data_set (int a[],int data_set_size){
 
 
 int main () {
-    static int * array = new int [100000000];
-    int data_Set_Size;
+    static int * array = new int [1000000000];
+    int data_Set_Size=100000;
     double elapsed;
     int i=1;
     clock_t start, end;
 
-    for(data_Set_Size=10000;elapsed<180;data_Set_Size+=20000) {
-        data_set(array, data_Set_Size);bubbleSort(array, data_Set_Size);
+    for(data_Set_Size=10000;elapsed<180;data_Set_Size+=500000) {
+        start = clock();
+        data_set(array, data_Set_Size);
+        insertionSort(array,data_Set_Size);
         end = clock();
         elapsed = double(end - start) / CLOCKS_PER_SEC;
         cout << "Time taken: "<< elapsed <<"   Current run: "<<i<<"  Size of Data Set: "<<data_Set_Size<<endl;
         i++;
     }
-	displayArray(array,data_Set_Size);
 
-
-	cout<<start<<" "<<end<<" "<<elapsed<<endl;
 	// write code here to perform experiments with the sort algorithms
 	
 	return 0;
