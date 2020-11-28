bbdd0ac2771d93b08e571bb7ea625beeaf5129a7
cumbercon97 <cumberson97@gmail.com>
2020-11-28 17:51:33 -0400

    Small changes made

--- Sorting.cpp
+++ Sorting.cpp
@@ -7,15 +7,7 @@
 #include "SortAlgorithms.h"
 
 using namespace std;
-void data_set (int a[],int data_set_size){
-    long long int numbers;
-    random_device rd;
-    default_random_engine generator(rd());
-    uniform_int_distribution<long long unsigned>distribution(0,100000000);
-    for (int i=1; i < data_set_size; i++){
-        a[i]= distribution(generator);
-    }
-}
+void data_set (int a[],int data_set_size);
 
 
 int main () {
@@ -25,7 +17,7 @@ int main () {
     int i=1;
     clock_t start, end;
 
-    for(data_Set_Size=10000;elapsed<180;data_Set_Size+=500000) {
+    for(data_Set_Size=10000; elapsed<180;data_Set_Size+=500000) {
         start = clock();
         data_set(array, data_Set_Size);
         insertionSort(array,data_Set_Size);
@@ -38,4 +30,13 @@ int main () {
 	// write code here to perform experiments with the sort algorithms
 	
 	return 0;
+}
+void data_set (int a[],int data_set_size){
+    long long int numbers;
+    random_device rd;
+    default_random_engine generator(rd());
+    uniform_int_distribution<long long unsigned>distribution(0,100000000);
+    for (int i=1; i < data_set_size; i++){
+        a[i]= distribution(generator);
+    }
 }
