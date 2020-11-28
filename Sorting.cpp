713605c67098ccf59340999ab51db4854eddbff8
cumbercon97 <cumberson97@gmail.com>
2020-11-28 18:28:58 -0400

    changes made

--- Sorting.cpp
+++ Sorting.cpp
@@ -8,19 +8,20 @@
 
 using namespace std;
 void data_set (int a[],int data_set_size);
-
+void Unsorted_Data_Test(int arr[],int Data_Set_Size,double elapsed,clock_t start,clock_t end);
+void Sorted_Data_Test(int arr[],int Data_Set_Size,double elapsed,clock_t start,clock_t end);
 
 int main () {
     static int * array = new int [1000000000];
-    int data_Set_Size=100000;
+    int data_Set_Size=10000;
     double elapsed;
     int i=1;
     clock_t start, end;
 
-    for(data_Set_Size=10000; elapsed<180;data_Set_Size+=500000) {
+    for(data_Set_Size=10000; elapsed<180;data_Set_Size+=100000) {
         start = clock();
         data_set(array, data_Set_Size);
-        insertionSort(array,data_Set_Size);
+        quickSort(array,0,data_Set_Size);
         end = clock();
         elapsed = double(end - start) / CLOCKS_PER_SEC;
         cout << "Time taken: "<< elapsed <<"   Current run: "<<i<<"  Size of Data Set: "<<data_Set_Size<<endl;
@@ -31,6 +32,8 @@ int main () {
 	
 	return 0;
 }
+
+
 void data_set (int a[],int data_set_size){
     long long int numbers;
     random_device rd;
@@ -39,4 +42,10 @@ void data_set (int a[],int data_set_size){
     for (int i=1; i < data_set_size; i++){
         a[i]= distribution(generator);
     }
+}
+void Unsorted_Data_Test(int arr[],int Data_Set_Size,double elapsed,clock_t start,clock_t end){
+
+}
+void Sorted_Data_Test(int arr[],int Data_Set_Size,double elapsed,clock_t start,clock_t end){
+
 }
